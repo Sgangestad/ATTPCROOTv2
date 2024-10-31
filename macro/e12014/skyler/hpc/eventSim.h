56d2f6d98cbe641fa53ad2df7c61a2319df798b2
Adam Anthony <aanthon2@highpoint.edu>
2024-10-31 15:54:17 -0400

    Add scripts for running slurm jobs (#2)
    * Add first slurm script
    
    * Add slurm script
    
    * Move all HPC files into folder
    
    * Refactor logs a bit so it's easier to use

---
+++ macro/e12014/skyler/hpc/eventSim.h
@@ -0,0 +1,432 @@
+/**
+ * This file contains infomration for using the AtSimpleSimulation class to simulate fission events.
+ * It mostly is responsible for generating fission events based on
+ *
+ */
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TFile.h"
+#include "TH2.h"
+#include "TLorentzVector.h"
+#include "TMath.h"
+#include "TRandom3.h"
+#include "TString.h"
+#include "TTree.h"
+
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <set>
+
+#include "Math/Vector3D.h"
+#include "Math/Vector4D.h"
+
+namespace fissionSim {
+
+// Define constants and short names for various classes.
+constexpr double AtoE = 939.0;
+constexpr double c = 2.998e8; // In m/s
+using VecXYZE = ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<>>;
+using PxPyPzEVector = ROOT::Math::PxPyPzEVector;
+using VecPolar = ROOT::Math::Polar3D<double>;
+using XYZVector = ROOT::Math::XYZVector;
+using Cartesian3D = ROOT::Math::Cartesian3D<>;
+using XYZPoint = ROOT::Math::XYZPoint;
+using vecInt = std::vector<Int_t>;
+using test = AtTools::AtKinematics; // Literally this is just to force ROOT to load the AtTools lib
+
+TF1 *fAsym = nullptr;   // Function describing the angular distribution of the fission fragments
+TF1 *fInvSin = nullptr; // Function describing the angular distribution of the fission fragments (in some limit)
+
+double angMin = TMath::Pi() / 2 - 1;
+double angMax = TMath::Pi() / 2 + 1;
+
+TF1 *csFit = nullptr;
+
+// This are the important parameters to set in the simulation
+int beamZ = 83;
+int beamA = 200;
+double beamM = 199.9332;
+float massFrac = 0.56;
+float massDev = 6;
+float decayAngle =
+   0; // Angle of the fission fragments in the CoM frame in radians. If 0, then sample from the angular distribution
+float zCutoff = 300; // Only simulate events from the window to here (dist from pad plane)
+
+// double beamOffsetX = -7.03043e+00;
+// double beamOffsetXsig = 1.62047e+01;
+// double beamOffsetY = 1.07526e+01;
+// double beamOffsetYsig = 1.51736e+01;
+
+double beamOffsetX = -0.3109;
+double beamOffsetXsig = 10.108;
+double beamOffsetY = -9.04402;
+double beamOffsetYsig = 8.08237;
+
+// double beamDirX = 6.93373e-03;
+// double beamDirXsig = 2.66458e-02;
+// double beamDirY = -1.79569e-02;
+// double beamDirYsig = 3.15214e-02;
+
+double beamDirX = 0.0186;
+double beamDirXsig = 0.002203;
+double beamDirY = 0.005707;
+double beamDirYsig = 0.002309;
+
+// double beamDirX1 = 0.00270859;
+double beamDirX1 = 0.0186;
+double beamDirX2 = -0.00106274;
+// double beamDirXsig1 = 0.0145266;
+double beamDirXsig1 = 0.00220254;
+double beamDirXsig2 = 0;
+// double beamDirY1 = -0.00245368;
+double beamDirY1 = 0.00571;
+double beamDirY2 = -0.00144553;
+// double beamDirYsig1 = 0.0149746;
+double beamDirYsig1 = 0.00230867;
+double beamDirYsig2 = 0;
+
+double beamE = 2.7000e+03;
+double beamEsig = 1.28122e+02;
+// double beamEsig = 0;
+
+double vertexZ;
+double vertexE;
+
+std::vector<std::pair<int, int>> ions;
+
+std::unique_ptr<AtSimpleSimulation> fSimulation{nullptr}; //!
+
+ofstream outFile;
+
+TH1D *simInfo = new TH1D("simInfo", "simInfo", 7, 0, 7);
+
+TString vX = "vX";
+TString vY = "vY";
+TString vZ = "vZ";
+TString vE = "vE";
+TString A0 = "A0";
+TString Z0 = "Z0";
+TString ang = "ang";
+
+/**  Function prototypes defined in this header files **/
+
+// Sample masses of FF given Acn and the gaussian distribution parameters massFrac and massDev
+vecInt getProducMasses(Int_t A, Float_t massFrac, Float_t massDev, TRandom *rand = new TRandom3());
+
+// Different ways of generating Z of FF given sampled masses
+vecInt getProductChargeSameDistro(Int_t Z, const vecInt &masses);
+vecInt getProductChargeMaxBE(Int_t Z, const vecInt &masses);
+vecInt getProductChargeMaxBEA(Int_t Z, const vecInt &masses);
+vecInt getProductChargeDist(Int_t Z, const vecInt &masses);
+
+// Return the four momenta of the products in the center of mass frame
+std::vector<VecXYZE> getProductMomenta(const vecInt &fragA, const vecInt &fragZ, TRandom *rand, VecPolar &decayAng);
+
+// Generate and simulate a fission event (end result is energy deposition in space in the TPC)
+// Returns true if the event should be accepted.
+bool generateEvent();
+void Init();    // Called at the start of the run
+void Exec();    // Called for each event
+void CleanUp(); // Called at the end of the run
+
+XYZVector SampleAsym()
+{
+   if (fAsym == nullptr) {
+      fAsym = new TF1("asym",
+                      "1 + [0] * cos(x) * cos(x) + [1] * cos(x) * cos(x) * cos(x) * cos(x)+ [2] * cos(x) * cos(x)* "
+                      "cos(x) * cos(x)* cos(x) * cos(x)",
+                      0, TMath::Pi());
+      fAsym->SetParameters(0.2676, 0.7282, 0.5472);
+   }
+
+   double theta = fAsym->GetRandom();
+   VecPolar decayAng(1, theta, gRandom->Uniform(TMath::TwoPi()));
+   return {decayAng.x(), decayAng.y(), decayAng.z()};
+}
+
+XYZVector SampleInvSin()
+{
+   if (fInvSin == nullptr) {
+      fInvSin = new TF1("invSin", "1 / sin(x)", angMin, angMax);
+   }
+
+   double theta = fInvSin->GetRandom();
+   VecPolar decayAng(1, theta, gRandom->Uniform(TMath::TwoPi()));
+   return {decayAng.x(), decayAng.y(), decayAng.z()};
+}
+
+void Init()
+{
+   fSimulation->RegisterBranch();
+   outFile.open("simOutFile.txt");
+
+   simInfo->GetXaxis()->SetBinLabel(1, vX);
+   simInfo->GetXaxis()->SetBinLabel(2, vY);
+   simInfo->GetXaxis()->SetBinLabel(3, vZ);
+   simInfo->GetXaxis()->SetBinLabel(4, vE);
+   simInfo->GetXaxis()->SetBinLabel(5, A0);
+   simInfo->GetXaxis()->SetBinLabel(6, Z0);
+   simInfo->GetXaxis()->SetBinLabel(7, ang);
+
+   FairRootManager *ioManager = FairRootManager::Instance();
+   if (ioManager == nullptr) {
+      LOG(error) << "Cannot find RootManager!" << std::endl;
+      return;
+   }
+
+   ioManager->Register("Info", "AtTPC", simInfo, true);
+}
+
+Int_t sumVector(const vecInt &vec)
+{
+   return std::accumulate(vec.cbegin(), vec.cend(), 0);
+}
+
+// Returns the average total kinetic energy from viola systematics in MeV
+double violaEn(int A, int Z)
+{
+   return 0.1189 * Z * Z / TMath::Power(A, 1.0 / 3.0) + 7.3;
+}
+
+bool generateEvent()
+{
+
+   for (int i = 0; i < 8; i++) {
+      simInfo->SetBinContent(i, 0);
+   }
+
+   XYZPoint beamOrigin;
+
+   XYZVector beamDirection;
+
+   /** Sample the beam direction, location, and energy and store it in the 4-vector beamMomentum*/
+   beamOrigin.SetX(gRandom->Gaus(beamOffsetX, beamOffsetXsig));
+   beamOrigin.SetY(gRandom->Gaus(beamOffsetY, beamOffsetYsig));
+   beamOrigin.SetZ(0);
+
+   beamDirection.SetX(gRandom->Gaus(beamDirX, beamDirXsig));
+   beamDirection.SetY(gRandom->Gaus(beamDirY, beamDirYsig));
+   beamDirection.SetZ(1);
+
+   auto originEnergy = gRandom->Gaus(beamE, beamEsig);
+
+   auto beamMomMag = sqrt(pow(originEnergy + beamM * AtoE, 2) - pow(beamM * AtoE, 2));
+   VecXYZE beamMomentum = AtTools::Kinematics::Get4Vector(beamMomMag * beamDirection.Unit(), beamM * AtoE);
+   // cout << "Beam Origin Energy: " << originEnergy << endl;
+   // cout << "Beam Origin: (" << beamOrigin.X() << ", " << beamOrigin.Y() << ", " << beamOrigin.Z() << ")" << endl;
+   // cout << "Beam Momentum: (" << beamMomentum.X() << ", " << beamMomentum.Y() << ", " << beamMomentum.Z() << ")" <<
+   // endl; cout << "Beam Energy: " << beamMomentum.E()  - beamMomentum.M() << endl;
+
+   // outFile << beamOrigin.X() << " " << beamOrigin.Y() << " " << beamOrigin.Z() << " ";
+
+   /** Sample the location where the reaction should occur in the detector (in z-dir in mm) */
+   auto vertexZ = gRandom->Uniform(1000 - zCutoff);
+   // vertexZ = 900;
+   cout << "Simulating beam to " << vertexZ << " mm" << endl;
+
+   // Simulate the beam to the reaction vertex. The lambda function is used to stop the simulation when the beam reaches
+   // the vertex. Returns the position and 4 momentum of the beam at the reaction vertex.
+   auto beamPos =
+      fSimulation->SimulateParticle(beamZ, beamA, beamOrigin, beamMomentum, [vertexZ](XYZPoint pos, VecXYZE mom) {
+         return pos.Z() < vertexZ - 10;
+         // return (mom.E() < vertexE);
+      });
+
+   //** Record data in output file and into the meta data histogram*/
+   outFile << beamOrigin.X() << " " << beamOrigin.Y() << " " << beamOrigin.Z() << " "
+           << beamMomentum.E() - beamMomentum.M() << " ";
+   outFile << beamPos.first.X() << " " << beamPos.first.Y() << " " << beamPos.first.Z() << " "
+           << beamPos.second.E() - beamPos.second.M() << " ";
+
+   simInfo->Fill(vX, beamPos.first.X());
+   simInfo->Fill(vY, beamPos.first.Y());
+   simInfo->Fill(vZ, beamPos.first.Z());
+   simInfo->Fill(vE, beamPos.second.E() - beamPos.second.M());
+
+   cout << "Beam Simulated" << endl;
+
+   cout << "Simulated vertex Position: (" << beamPos.first.X() << ", " << beamPos.first.Y() << ", "
+        << 1000 - beamPos.first.Z() << ")" << endl;
+   cout << "Simulated vertex Energy: " << beamPos.second.E() - beamPos.second.M() << endl;
+   cout << "Number of hits registered: " << fSimulation->GetNumPoints() << endl;
+
+   vecInt fragZ = {0, 0};
+   vecInt fragA = {0, 0};
+
+   // Generate the masses and charges of the fission fragments
+   while (fragZ[0] == 0) {
+      fragA = getProducMasses(beamA + 4, massFrac, massDev, gRandom);
+      fragZ = getProductChargeDist(beamZ + 2, fragA);
+      std::cout << "Trying " << fragA[0] << " " << fragZ[0] << std::endl;
+   }
+
+   // Generate an array of products in the rest frame using the supplied angle
+   // or a random angle if 0 was supplied
+   VecPolar decayDir(1, decayAngle, 0); // r, theta, phi
+
+   auto decayMomenta = getProductMomenta(fragA, fragZ, gRandom, decayDir);
+
+   outFile << fragA[0] << " " << fragZ[0] << " " << decayDir.Theta() << " ";
+
+   simInfo->Fill(A0, fragA[0]);
+   simInfo->Fill(Z0, fragZ[0]);
+   simInfo->Fill(ang, decayDir.Theta());
+
+   auto fBeamBoost = ROOT::Math::Boost(beamPos.second.BoostToCM());
+   fBeamBoost.Invert();
+
+   std::array<std::pair<XYZPoint, PxPyPzEVector>, 2> finalPos;
+   bool keepEvent = true;
+   double radCut = 50; // in mm
+   for (int i = 0; i < 2; i++) {
+      auto labP = fBeamBoost(decayMomenta[i]);
+
+      cout << "Simulating fragment " << i << endl;
+      // cout << "fragZ.size() " << fragZ.size() << "; fragA.size()" << fragA.size() << endl;
+      cout << "Z: " << fragZ[i] << ", A: " << fragA[i] << " at " << labP.E() - labP.M() << " MeV, "
+           << (labP.E() - labP.M()) / fragA[i] << "MeV/A" << endl;
+      auto [fragPos, fragMom] = fSimulation->SimulateParticle(fragZ[i], fragA[i], beamPos.first, labP);
+
+      // Only keep events that would have passed our trigger condition
+      double fragRho = std::sqrt(fragPos.X() * fragPos.X() + fragPos.Y() * fragPos.Y());
+      keepEvent &= fragRho > radCut;
+
+      cout << "Number of hits registered: " << fSimulation->GetNumPoints() << endl;
+   }
+   return keepEvent;
+}
+
+vecInt getProductChargeMaxBE(Int_t Z, const vecInt &masses)
+{
+   auto asym = 23.2;
+   auto coul = 0.714;
+   Int_t A = sumVector(masses);
+
+   Float_t numerator = 8 * asym * masses.at(0) * Z;
+   numerator += coul * TMath::Power(masses.at(0), 2.0 / 3.0) * masses.at(1);
+   numerator += coul * TMath::Power(masses.at(1), 2.0 / 3.0) * masses.at(0) * (2 * Z - 1);
+
+   Float_t denominator = 8 * asym * A;
+   denominator += 2 * coul * TMath::Power(masses.at(1), 2.0 / 3.0) * masses.at(0);
+   denominator += 2 * coul * TMath::Power(masses.at(0), 2.0 / 3.0) * masses.at(1);
+
+   Int_t Z1 = TMath::Nint(numerator / denominator);
+   vecInt fragZ;
+   fragZ.push_back(Z1);
+   fragZ.push_back(Z - Z1);
+   return fragZ;
+}
+
+vecInt getProductChargeDist(Int_t Z, const vecInt &masses)
+{
+   std::vector<int> distZ;
+   for (auto [Z, A] : ions) {
+      if (A == masses[0])
+         distZ.push_back(Z);
+   }
+
+   if (distZ.size() > 0) {
+      auto Z0 = distZ[gRandom->Integer(distZ.size())];
+      auto Z1 = Z - Z0;
+      return {Z0, Z1};
+   }
+
+   return {0, 0};
+}
+
+vecInt getProductChargeMaxBEA(Int_t Z, const vecInt &masses)
+{
+   // LDM parameters
+   auto asym = 23.2;
+   auto coul = 0.714;
+
+   Int_t A = sumVector(masses);
+   auto A1 = masses.at(0);
+   auto A2 = masses.at(1);
+
+   Float_t numAsym = A2 * A2 - A1 * A2 + 2 * Z * A1;
+   Float_t numerator = 4 * asym * A1 * numAsym;
+
+   numerator += coul * TMath::Power(A1, 2.0 / 3.0) * A2 * A2;
+   numerator += coul * TMath::Power(A2, 2.0 / 3.0) * A1 * A1 * (2 * Z - 1);
+
+   Float_t denominator = 8 * asym * (A1 * A1 + A2 * A2);
+   denominator += 2 * coul * TMath::Power(A2, 2.0 / 3.0) * A1 * A1;
+   denominator += 2 * coul * TMath::Power(A1, 2.0 / 3.0) * A2 * A2;
+
+   Int_t Z1 = TMath::Nint(numerator / denominator);
+   vecInt fragZ;
+   fragZ.push_back(Z1);
+   fragZ.push_back(Z - Z1);
+   return fragZ;
+}
+
+std::vector<VecXYZE> getProductMomenta(const vecInt &fragA, const vecInt &fragZ, TRandom *rand, VecPolar &decayAng)
+{
+
+   Int_t Z = sumVector(fragZ);
+   Int_t A = sumVector(fragA);
+
+   // Get the masses of the fragments in MeV
+   double m[2];
+   for (int i = 0; i < 2; ++i)
+      m[i] = fragA.at(i) * AtoE;
+
+   // Get the KE of the products
+   double kE = violaEn(A, Z);
+
+   double gamma1 = AtTools::Kinematics::GetGamma(kE, m[0], m[1]);
+   double p1 = AtTools::Kinematics::GetRelMom(gamma1, m[0]);
+
+   // Set the momentum unit vector if nullptr
+   if (decayAng.Theta() != 0)
+      decayAng.SetPhi(rand->Uniform(TMath::TwoPi()));
+   else {
+      decayAng = SampleAsym();
+      // decayAng = SampleInvSin();
+   }
+
+   cout << "Theta in CoM: " << decayAng.Theta() * TMath::RadToDeg() << " deg" << endl;
+
+   // Set the momentum of first particles
+   // decayAng.SetR(TMath::Sqrt(E[0] * E[0] - m[0] * m[0]));
+   decayAng.SetR(p1);
+   ROOT::Math::XYZVector pDir(decayAng);
+
+   std::vector<VecXYZE> ret;
+   ret.push_back(AtTools::Kinematics::Get4Vector(pDir, m[0]));
+   ret.push_back(AtTools::Kinematics::Get4Vector(-pDir, m[1]));
+
+   return ret;
+}
+
+vecInt getProducMasses(Int_t A, Float_t massFrac, Float_t massDev, TRandom *rand)
+{
+   Int_t A1 = TMath::Nint(rand->Gaus(A * massFrac, massDev));
+   if (A1 < A / 2)
+      A1 = A - A1;
+   vecInt fragA;
+   fragA.push_back(A1);
+   fragA.push_back(A - A1);
+   return fragA;
+}
+
+void Exec()
+{
+
+   while (true) {
+      fSimulation->NewEvent();
+      if (generateEvent())
+         break;
+      cout << "Failed to generate event that passes trigger. Trying again." << std::endl;
+   }
+   outFile << endl;
+}
+
+void CleanUp()
+{
+   outFile.close();
+}
+
+} // namespace fissionSim
---
+++ macro/e12014/skyler/hpc/slurm_fit_sim.C
@@ -0,0 +1,234 @@
+// Code to take MC tracks and digitize
+// #include "eventCombine.h"
+#include "DigiSimInfo.h"
+#include "response.h"
+
+bool reduceFunc(AtRawEvent *evt);
+
+void slurm_fit_sim(int runNum, int numThreads, bool saveRawEvent)
+{
+   auto verbSpec =
+      fair::VerbositySpec::Make(fair::VerbositySpec::Info::severity, fair::VerbositySpec::Info::file_line_function);
+   fair::Logger::DefineVerbosity("user1", verbSpec);
+   fair::Logger::SetVerbosity("user1");
+
+   TString inOutDir = "./data/";
+   // TString outputFile = inOutDir + "output_digiLg.root";
+   TString outputFile = inOutDir + TString::Format("output_digi%02d.root", runNum);
+   outputFile = inOutDir + TString::Format("output_digi%02d.root", runNum);
+   TString scriptfile = "e12014_pad_mapping.xml";
+   TString paramFile = "ATTPC.e12014.par";
+
+   TString dir = getenv("VMCWORKDIR");
+
+   // TString mcFile = "./data/sim_attpc.root";
+   // TString mcFile = inOutDir + "symFissionLg.root";
+   TString mcFile = inOutDir + TString::Format("simFission%02d.root", runNum);
+   TString sharedInfoDir =
+      "/home/faculty/aanthony/fission/data/e12014/tpcSharedInfo/"; // Directory containing the shared information for
+                                                                   // the TPC
+
+   // Create the full parameter file paths
+   // TString digiParFile = dir + "/parameters/" + paramFile;
+   TString digiParFile = paramFile;
+   TString mapParFile = dir + "/scripts/" + scriptfile;
+
+   // -----   Timer   --------------------------------------------------------
+   TStopwatch timer;
+
+   // ------------------------------------------------------------------------
+
+   // __ Run ____________________________________________
+   FairRunAna *fRun = new FairRunAna();
+   FairFileSource *source = new FairFileSource(mcFile);
+   fRun->SetSource(source);
+   fRun->SetOutputFile(outputFile);
+
+   FairRuntimeDb *rtdb = fRun->GetRuntimeDb();
+   FairParAsciiFileIo *parIo1 = new FairParAsciiFileIo();
+   parIo1->open(digiParFile.Data(), "in");
+   rtdb->setFirstInput(parIo1);
+
+   // Create the detector map to pass to the simulation
+   auto mapping = std::make_shared<AtTpcMap>();
+   mapping->ParseXMLMap(mapParFile.Data());
+   mapping->GeneratePadPlane();
+
+   std::ifstream file(sharedInfoDir + "/e12014_zap.csv");
+   if (!file.is_open())
+      LOG(error) << "Failed to open smart zap file";
+
+   // Clear out the header
+   std::string temp;
+   std::getline(file, temp);
+   std::getline(file, temp);
+
+   for (auto &row : CSVRange<int>(file)) {
+      LOG(debug) << "Inhibiting " << row[4];
+      mapping->InhibitPad(row[4], AtMap::InhibitType::kLowGain);
+   }
+
+   // __ AT digi tasks___________________________________
+   // AtClusterizeTask *clusterizer = new AtClusterizeTask(std::make_shared<AtClusterize>());
+   AtClusterizeTask *clusterizer = new AtClusterizeTask(std::make_shared<AtClusterizeLine>());
+   clusterizer->SetPersistence(false);
+
+   // AtPulseLineTask *pulse = new AtPulseLineTask();
+
+   // AtPulseTask *pulse = new AtPulseTask(std::make_shared<AtPulse>(mapping));
+   auto pulse = std::make_shared<AtPulseLine>(mapping, Response::GetResponse);
+   pulse->SetSaveCharge(saveRawEvent);
+   pulse->SetLowGain(0.19);
+   // pulse->SetLowGain(1);
+   Response::scaling = 0.01 * 0.75;
+   AtPulseTask *pulseTask = new AtPulseTask(pulse);
+   pulseTask->SetPersistence(saveRawEvent);
+
+   /*AtMacroTask *combTask = new AtMacroTask();
+   combTask->AddInitFunction(eventComb::init);
+   combTask->AddFunction(eventComb::combine);*/
+
+   /**** PSA Task ****/
+   AtRawEvent *respAvgEvent;
+   TFile *f2 = new TFile(sharedInfoDir + "respAvg.root");
+   f2->GetObject("avgResp", respAvgEvent);
+   f2->Close();
+
+   auto threshold = 50;
+   auto maxThreshold = 1500;
+
+   auto psa = std::make_unique<AtPSADeconvFit>();
+   psa->SetResponse(*respAvgEvent);
+   psa->SetThreshold(15); // Threshold in charge units
+   psa->SetFilterOrder(6);
+   psa->SetCutoffFreq(75);
+   psa->SetThreshold(10);
+   auto psaBeam = std::make_unique<AtPSATBAvg>();
+   psaBeam->SetThreshold(45);
+   auto psaComp = std::make_unique<AtPSAComposite>(std::move(psaBeam), std::move(psa), 20);
+   AtPSAtask *psaTask = new AtPSAtask(std::move(psaComp));
+   psaTask->SetInputBranch("AtRawEvent");
+   psaTask->SetOutputBranch("AtEvent");
+   psaTask->SetPersistence(true);
+
+   /**** Space charge correction ****/
+   // auto SCModel = std::make_unique<AtRadialChargeModel>(E12014SC(nsclRunNum));
+   auto SCModel = std::make_unique<AtLineChargeModel>();
+   // SCModel->SetLambda(E12014SC(nsclRunNum).GetLambda());
+   //  SCModel->SetStepSize(0.1);
+   // SCModel->SetBeamLocation({-7, 10, 0}, {0, 7, 1000});
+   SCModel->SetBeamLocation({0, -6, 0}, {10, 0, 1000});
+   auto scTask = new AtSpaceChargeCorrectionTask(std::move(SCModel));
+   scTask->SetInputBranch("AtEvent");
+   scTask->SetOutputBranch("AtEventCorr");
+
+   /**** Y-pattern fit ****/
+   auto method = std::make_unique<SampleConsensus::AtSampleConsensus>(
+      SampleConsensus::Estimators::kYRANSAC, AtPatterns::PatternType::kFission, RandomSample::SampleMethod::kY);
+   method->SetDistanceThreshold(20);
+   method->SetNumIterations(500);
+   method->SetMinHitsPattern(150);
+   method->SetChargeThreshold(10); //-1 implies no charge-weighted fitting
+   method->SetFitPattern(true);
+   auto sacTask = new AtSampleConsensusTask(std::move(method));
+   sacTask->SetPersistence(false);
+   sacTask->SetInputBranch("AtEvent");
+   sacTask->SetOutputBranch("AtPatternEvent");
+
+   /******** Create fission task ********/
+   AtFissionTask *fissionTask = new AtFissionTask(0);
+   fissionTask->SetUncorrectedEventBranch("AtEvent");
+   fissionTask->SetPatternBranch("AtPatternEvent");
+   fissionTask->SetOutBranch("AtFissionEvent");
+   fissionTask->SetPersistance(true);
+
+   AtMacroTask *infoTask = new AtMacroTask();
+   infoTask->AddInitFunction(digiSimInfo::Init);
+
+   fRun->AddTask(clusterizer); // This task turns energy into electons in the gas
+   fRun->AddTask(pulseTask);   // This task turns electorns in gas to traces on the pad plabe
+   fRun->AddTask(psaTask);     // This turns waveforms on pads into hits in space
+   fRun->AddTask(sacTask);     // This task finds the fisison Y-pattern
+   fRun->AddTask(fissionTask); // This task assembles the fisison events
+   fRun->AddTask(infoTask);    //
+
+   /** Begin MC Fit **/
+   int Zcn = 83 + 2;  // Number of protons in the compound nucleus
+   int Acn = 200 + 4; // Number of nucleons in the compound nucleus
+   int Zmin = 26;     // Minimum Z to simulate for the fission fragments
+   int Zmax = 59;     // Maximum Z to simulate for hte fission fragments
+
+   TString geoFile = "ATTPC_v1.1_geomanager.root";
+   TString GeoDataPath = dir + "/geometry/" + geoFile;
+   TString energyLossDir = sharedInfoDir + "/eLoss/"; // Directory containing the energy loss tables
+
+   E12014::fMap = mapping;
+   //  __ Init and run ___________________________________
+
+   // Create underlying simulation class
+   auto sim = std::make_shared<AtSimpleSimulation>(GeoDataPath.Data());
+
+   auto scModel = std::make_shared<AtRadialChargeModel>(nullptr);
+   scModel->SetStepSize(0.1);
+   scModel->SetBeamLocation({0, -6, 0}, {10, 0, 1000});
+   // sim->SetSpaceChargeModel(scModel);
+
+   // Create and load energy loss models
+   std::vector<std::pair<int, int>> ions;
+   for (int i = Zmin; i <= Zmax; i++)
+      ions.push_back(
+         {i, std::round((double)i / Zcn * Acn)}); // Calculate the number of nucleons for each Z and add to the list
+
+   // For all our ions, load the energy loss tables
+   for (auto [Z, A] : ions) {
+      auto eloss = std::make_shared<AtTools::AtELossTable>();
+      eloss->LoadLiseTable(TString::Format(energyLossDir + "/LISE/%d_%d.txt", Z, A).Data(), A,
+                           0); // Note a different function call will be needed if loading SRIM tables
+      sim->AddModel(Z, A, eloss);
+   }
+
+   auto cluster = std::make_shared<AtClusterizeLine>();
+   auto pulseSim = std::make_shared<AtPulseLine>(mapping);
+   pulseSim->SetSaveCharge(true);
+   auto psa2 = std::make_shared<AtPSADeconvFit>();
+   psa2->SetUseSimCharge(true);
+   psa2->SetThreshold(25);
+
+   auto fitter = std::make_shared<MCFitter::AtMCFission>(sim, cluster, pulseSim);
+   fitter->SetPSA(psa2);
+   fitter->SetNumIter(100);
+   fitter->SetNumThreads(numThreads);
+   fitter->SetNumRounds(2);
+
+   AtMCFitterTask *fitTask = new AtMCFitterTask(fitter);
+   fitTask->SetPatternBranchName("AtFissionEvent");
+
+   fRun->AddTask(fitTask);
+
+   Response::Init();
+   fRun->Init();
+
+   timer.Start();
+   // fRun->Run(0, 20001);
+   fRun->Run();
+   timer.Stop();
+
+   std::cout << std::endl << std::endl;
+   std::cout << "Macro finished succesfully." << std::endl << std::endl;
+   // -----   Finish   -------------------------------------------------------
+
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   cout << endl;
+   cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+   cout << endl;
+   // ------------------------------------------------------------------------
+}
+
+bool reduceFunc(AtRawEvent *evt)
+{
+   /*if(evt->GetEventID() % 2 == 0)
+      return false;
+   return (evt->GetNumPads() > 0) && evt->IsGood();*/
+   return true;
+}
