977d727c2f7a6b87c518884026d5c587947d2ac3
Adam Anthony <aanthon2@highpoint.edu>
2024-10-31 13:06:25 -0400

    Refactor logs a bit so it's easier to use

--- macro/e12014/skyler/hpc/slurm-submit.sh
+++ macro/e12014/skyler/hpc/slurm-submit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=sim_fission_fit
-#SBATCH --output=log/sim_fission_%j.out
-#SBATCH --error=log/sim_fission_%j.err
+#SBATCH --output=log/run_%j/log.out
+#SBATCH --error=log/run_%j/log.out
 #SBATCH --partition=cs
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=UNLIMITED
-#SBATCH --chdir=/home/faculty/aanthony/fission/adam/ATTPCROOTv2/macro/e12014/skyler
+#SBATCH --chdir=/home/faculty/aanthony/fission/adam/ATTPCROOTv2/macro/e12014/skyler/hpc
 
 # This macro will generate fisison events and then fit them using the MC fitting code
 # The number of events is set by the NUM_EVENTS variable
@@ -17,17 +17,18 @@
 
 NUM_EVENTS=1
 SAVE_RAW_EVENT=true
+LOG_FOLDER="log/run_${SLURM_JOB_ID}/"
 
 echo "For run number  ${SLURM_JOB_ID}"
 echo "Starting to simulate fission events at  $(date)"
 
 # The following command will generate the fission events
-root -l -q -b "run_sim_fiss_slurm.C(${SLURM_JOB_ID}, ${NUM_EVENTS})"
+root -l -q -b "slurm_sim_fiss.C(${SLURM_JOB_ID}, ${NUM_EVENTS})" &> ${LOG_FOLDER}/sim.log
 
 echo "Finished to simulating fission events at  $(date)"
 echo "Starting to fit fission events at  $(date) with ${SLURM_CPUS_ON_NODE} threads"
 
 # The following command will fit the fission events
-root -l -q -b "run_sim_fit_slurm.C(${SLURM_JOB_ID}, ${SLURM_CPUS_ON_NODE}, ${SAVE_RAW_EVENT})"
+root -l -q -b "slurm_fit_sim.C(${SLURM_JOB_ID}, ${SLURM_CPUS_ON_NODE}, ${SAVE_RAW_EVENT})" &> ${LOG_FOLDER}/fit.log
 
 echo "Finished fitting fission events at  $(date)"
